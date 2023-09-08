26cf569ac25562a2d406240c51638c904be0f2b9
Abhimanyu <abhimanyu.s@tekizma.com>
2023-09-08 13:33:34 +0530

    Updated-Files

--- src/data/constants.js
+++ src/data/constants.js
@@ -265,7 +265,7 @@ export const projects = [
     description:
       "Welcome to my portfolio! This project showcases my skills, projects, and experiences as a web developer.",
     image:
-      "https://raw.githubusercontent.com/s-abhimanyu/Images/main/Portfolio.png",
+      "https://raw.githubusercontent.com/s-abhimanyu/Images/main/Portfolio2.png",
     tags:[
       "Js",
       "HTML",
