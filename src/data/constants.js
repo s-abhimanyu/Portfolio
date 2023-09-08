82e9789af5e760352c48eadc3fc7c76fbaa52319
Abhimanyu <abhimanyu.s@tekizma.com>
2023-09-08 12:33:08 +0530

    Updated-Files

---
+++ src/data/constants.js
@@ -0,0 +1,269 @@
+export const Bio = {
+  name: "S Abhimanyu",
+  roles: [
+    "Programmer",
+    "Full Stack Developer",
+  ],
+  description:
+    "I am a motivated and skilled programmer with a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
+  github: "https://github.com/s-abhimanyu",
+  resume:
+    "https://github.com/s-abhimanyu/Resume/blob/main/Abhimanyu%20CV.pdf",
+  linkedin: "https://www.linkedin.com/in/s-abhimanyu-95356b249/",
+  insta: "https://www.instagram.com/_s.abhimanyu_/",
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "React Js",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+      {
+        name: "Php",
+        image:
+          "https://raw.githubusercontent.com/s-abhimanyu/Images/main/PHP-logo.svg.png",
+      }
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+      {
+        name: "Node Js",
+        image: "https://nodejs.org/static/images/logo.svg",
+      },
+      {
+        name: "Python",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+      },
+      {
+        name: "MySQL",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+      },
+      {
+        name: "MongoDB",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg",
+      },
+      {
+        name: "PhpMyAdmin",
+        image: "https://raw.githubusercontent.com/PradeepKumar368/Photos/main/PhpMyAdmin.png",
+      },
+    ],
+  },
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "Git",
+        image:
+          "https://camo.githubusercontent.com/fbfcb9e3dc648adc93bef37c718db16c52f617ad055a26de6dc3c21865c3321d/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f6769742d73636d2f6769742d73636d2d69636f6e2e737667",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+      {
+        name: "Docker",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/docker/docker-original-wordmark.svg",
+      },
+      {
+        name: "Jenkins",
+        image:
+          "https://raw.githubusercontent.com/s-abhimanyu/Images/main/jenkins_image.png",
+      },
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+      {
+        name: "Postman",
+        image:
+          "https://camo.githubusercontent.com/93b32389bf746009ca2370de7fe06c3b5146f4c99d99df65994f9ced0ba41685/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f676574706f73746d616e2f676574706f73746d616e2d69636f6e2e737667",
+      },
+      {
+        name: "Linux",
+        image:
+          "https://raw.githubusercontent.com/s-abhimanyu/Images/main/linux.png",
+      },
+      {
+        name: "AWS",
+        image:
+          "https://raw.githubusercontent.com/s-abhimanyu/Images/main/aws.png",
+      },
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    img: "https://raw.githubusercontent.com/s-abhimanyu/Images/main/1519864754320.jpg",
+    role: "DevOps & Data Analyst",
+    company: "Tekizma Inc.",
+    date: "Dec 2022 - Apr 2023",
+    desc: "Implemented Logistic-Regression, Kmeans-Clustering & several other Machine Learning Algorithms for Big Data using Pyspark MLlib in Datanyx App. Executed CD/CI for the Datanyx App with the Help of Jenkins.",
+    skills: [
+      "Machine Learning",
+      "Docker",
+      "Git",
+      "Git Hub",
+      "AWS",
+      "Pyspark",
+      "Jenkins",
+    ],
+  },
+];
+
+export const education = [
+  {
+    id: 0,
+    img: "https://raw.githubusercontent.com/s-abhimanyu/Images/main/IIITG.png",
+    school: "IIIT-G ( Indian Institute of Information Technology - Guwahati) B-Tech in Computer Science And Engineering.",
+    date: "Dec 2020 - Jun 2024",
+    grade: "7.08 CGPA",
+    desc: "I am currently pursuing a Bachelor's degree in Computer Science and Engineering atIndian Institute of Information Technology - Guwahati. I have completed 6 semesters and have a CGPA of 7.08. I have taken courses in Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Operating Systems, and Computer Networks, among others.",
+    degree: "Bachelor of Technology - BTech, Computer Science and Engineering",
+  },
+  {
+    id: 1,
+    img: "https://raw.githubusercontent.com/s-abhimanyu/Images/main/channels4_profile.jpg",
+    school: "Sri Chaitanya Junior Kalasala - TL , Intermediate Education ",
+    date: "Apr 2019 - Apr 2020",
+    grade: "9.4 CGPA",
+    desc: "I completed my class 12 high school education at Sri Chaitanya Junior Kalasala - TL, where I studied Science with Mathematics.",
+    degree: "SSC(XII), Science with Mathematics",
+  },
+  {
+    id: 2,
+    img: "https://raw.githubusercontent.com/s-abhimanyu/Images/main/kv.png",
+    school: "Kendriya Vidyalaya University of Hyderabad Campus - TL",
+    date: "Apr 2017 - Apr 2018",
+    grade: "7.2 CGPA",
+    desc: "I completed my class 10 education at Kendriya Vidyalaya University of Hyderabad Campus - TL.",
+    degree: "CBSE(X)",
+  },
+];
+
+export const projects = [
+  {
+    id: 0,
+    title: "OnlineFlightBooking-PHP",
+    date: "Aug 2022 - Nov 2022",
+    description:
+      "Created a Software application that is similar to the process of Web Login before Flight Boarding , Implemented a Payment Gateway for the Completion of Ticket & Purchase And Printing the Bording Pass.",
+    image:
+      "https://raw.githubusercontent.com/s-abhimanyu/Images/main/Online%20Flight%20Booking%20and%209%20more%20pages%20-%20Personal%20-%20Microsoft%E2%80%8B%20Edge%209_7_2023%204_43_04%20PM.png",
+    tags: [
+      "PHP",
+      "PhpMyAdmin",
+      "CSS",
+      "HTML",
+      "Js",
+      "MySQL",
+      "Phpmailer",
+    ],
+    category: "Software App",
+    github: "https://github.com/s-abhimanyu/OnlineFlightBooking-PHP",
+    member: [
+      {
+        name: "S Abhimanyu",
+        img: "https://raw.githubusercontent.com/s-abhimanyu/Images/main/HeroImage.jpg",
+        linkedin: "https://www.linkedin.com/in/s-abhimanyu-95356b249/",
+        github: "https://github.com/s-abhimanyu/",
+      },
+      {
+        name: "Pradeep Kumar",
+        img: "https://raw.githubusercontent.com/PradeepKumar368/Photos/main/Pradeep.jpg",
+        linkedin: "https://www.linkedin.com/in/pradeepkumar68/",
+        github: "https://github.com/PradeepKumar368?tab=repositories/",
+      },
+    ],
+  },
+  {
+    id: 1,
+    title: "Software-Microsoft Power Bi",
+    date: "Jan 2022 - March 2022",
+    description:
+      " Data Analysis - Imported Data From Global Marketing Super Store (2016) And Made a Interactive Dashboard With The Help of Microsoft Power-Bi.",
+    image:
+      "https://raw.githubusercontent.com/s-abhimanyu/Images/main/Screenshot%202023-09-07%20171252.png",
+    tags: ["Power Bi"],
+    category: "Software App",
+    github: "https://github.com/s-abhimanyu/Power-Bi",
+  },
+  {
+    id: 2,
+    title: "Customer feedback Website - PHP",
+    date: "Jan 2022 - March 2022",
+    description:
+      "Hosted a Customer Feedback Website using Amazon-AWS (Amazon Web Services) , Integrated AWS-RDS, EC2 (Elastic-Cloud-Computing) & other components to host the Website.",
+    image:
+      "https://raw.githubusercontent.com/s-abhimanyu/Images/main/feedback-website.png",
+    tags: [
+      "PHP",
+      "AWS",
+      "AWS-RDS",
+      "MySql",
+      "EC2",
+      "S3 Bucket",
+    ],
+    category: "Cloud App",
+    github: "{https://github.com/s-abhimanyu/Customer_Feedback_Website",
+    member: [
+      {
+        name: "S Abhimanyu",
+        img: "https://raw.githubusercontent.com/s-abhimanyu/Images/main/HeroImage.jpg",
+        linkedin: "https://www.linkedin.com/in/s-abhimanyu-95356b249/",
+        github: "https://github.com/s-abhimanyu/",
+      },
+      {
+        name: "Pradeep Kumar",
+        img: "https://raw.githubusercontent.com/PradeepKumar368/Photos/main/Pradeep.jpg",
+        linkedin: "https://www.linkedin.com/in/pradeepkumar68/",
+        github: "https://github.com/PradeepKumar368?tab=repositories/",
+      },
+    ],
+  },
+  {
+    id: 3,
+    title: "Personalized Portfolio",
+    date: "May 2023 - Jun 2023",
+    description:
+      "Welcome to my portfolio! This project showcases my skills, projects, and experiences as a web developer.",
+    image:
+      "https://raw.githubusercontent.com/s-abhimanyu/Images/main/Portfolio.png",
+    tags:[
+      "Js",
+      "HTML",
+      "CSS",
+      "ReactJs",
+      "Style-Components",
+    ],
+    category: "Web App",
+    github: "https://github.com/s-abhimanyu/Power-Bi",
+  },
+];
